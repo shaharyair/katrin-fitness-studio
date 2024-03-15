e6492555495cbdaea405139617e1c47e9fee60a1
Shahar Yair <shaharyair12@gmail.com>
2024-03-15 12:32:52 +0200

    component: refactor: remove animations from transformations
    - use grid for layout 
    - make images responsive using next image

--- src/components/Transformations.jsx
+++ src/components/Transformations.jsx
@@ -1,25 +1,11 @@
-import { motion } from "framer-motion";
 import Image from "next/image";
 import { urlFor } from "../../utils/sanity/imageUrlBuilder";
-
-const transformationVariants = {
-  hidden: { opacity: 0, y: 75 },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      ease: "easeInOut",
-      duration: 0.5,
-      delay: 0.25,
-    },
-  },
-};
 
 export default function Transformations({ content }) {
   const { text, assets } = content;
 
   return (
-    <section className="w-full bg-black py-10" id="before-and-after">
+    <section className="w-full bg-black py-10" id="transformations">
       <div className="container flex flex-col items-center justify-center gap-4 lg:gap-8">
         {/* title and subtitle */}
         <div className="flex flex-col items-center justify-center gap-2 text-center text-xl font-thin text-white lg:gap-4 lg:text-3xl">
@@ -30,48 +16,33 @@ export default function Transformations({ content }) {
         </div>
 
         {/* gallery */}
-        <div className="flex h-full w-full flex-wrap items-center justify-center">
+        <div className=" grid place-items-center gap-2 lg:grid-cols-2 lg:gap-4">
           {assets.map((assetObj) => (
             <div
-              className="flex h-full w-full items-center justify-center lg:w-1/2"
               key={assetObj?._id}
+              className="flex items-center justify-center gap-1 lg:gap-2"
             >
               {/* after transformation image */}
-              <motion.div
-                variants={transformationVariants}
-                initial="hidden"
-                whileInView="visible"
-                viewport={{ once: true }}
-                className="aspect-[1/1.15] h-full w-full px-0.5 py-1 lg:p-1"
-              >
-                <Image
-                  src={urlFor(assetObj?.afterImage).url()}
-                  width={500}
-                  height={500}
-                  alt={`After Image ${assetObj?.title}`}
-                  className="h-full w-full rounded-lg border-[.5px] border-primary object-cover object-center drop-shadow-md"
-                  loading="lazy"
-                />
-              </motion.div>
+              <Image
+                src={urlFor(assetObj?.afterImage).url()}
+                width={450}
+                height={500}
+                alt={`After Image ${assetObj?.title}`}
+                sizes="(min-width: 1024px) 20vw, 40vw"
+                className="aspect-[1/1.15] rounded-lg border-[.5px] border-primary object-cover object-center drop-shadow-md"
+                loading="lazy"
+              />
 
               {/* before transformation image */}
-              <motion.div
-                variants={transformationVariants}
-                initial="hidden"
-                whileInView="visible"
-                viewport={{ once: true }}
-                className="aspect-[1/1.15] h-full w-full px-0.5 py-1 lg:p-1"
-                key={assetObj?.id}
-              >
-                <Image
-                  src={urlFor(assetObj?.beforeImage).url()}
-                  width={500}
-                  height={500}
-                  alt={`Before Image ${assetObj?.title}`}
-                  className="h-full w-full rounded-lg border-[.5px] border-primary object-cover object-center drop-shadow-md"
-                  loading="lazy"
-                />
-              </motion.div>
+              <Image
+                src={urlFor(assetObj?.beforeImage).url()}
+                width={450}
+                height={500}
+                alt={`Before Image ${assetObj?.title}`}
+                sizes="(min-width: 1024px) 20vw, 40vw"
+                className="aspect-[1/1.15] rounded-lg border-[.5px] border-primary object-cover object-center drop-shadow-md"
+                loading="lazy"
+              />
             </div>
           ))}
         </div>
