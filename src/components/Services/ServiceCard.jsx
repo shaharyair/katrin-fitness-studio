3b37b745635ca0eeaeceefd9e9ff99ae1f93402f
Shahar Yair <shaharyair12@gmail.com>
2024-03-15 12:36:19 +0200

    component: refactor: remove animations from service card

--- src/components/Services/ServiceCard.jsx
+++ src/components/Services/ServiceCard.jsx
@@ -1,35 +1,17 @@
-import { motion } from "framer-motion";
 import Image from "next/image";
 import { cn } from "../../../utils/cn";
 import { urlFor } from "../../../utils/sanity/imageUrlBuilder";
-
-const serviceVariants = {
-  hidden: { opacity: 0, y: 100 },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      duration: 0.5,
-      delay: 0.25,
-    },
-  },
-};
 
 export default function ServiceCard(props) {
   const { content } = props;
 
   return (
-    <motion.div
-      variants={serviceVariants}
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: true }}
-      className="h-full w-full"
-    >
+    <div>
       <Image
         src={urlFor(content?.mainImage).url()}
         width={750}
         height={450}
+        sizes="100vw"
         alt="Services Image"
         className={cn(
           "mb-2 aspect-[1/1.15] h-full w-full rounded-lg object-cover object-center drop-shadow-md",
@@ -39,6 +21,6 @@ export default function ServiceCard(props) {
       />
       <h2 className="text-xl text-primary lg:text-2xl">{content?.title}</h2>
       <p className="text-lg font-thin lg:text-xl">{content?.subtitle}</p>
-    </motion.div>
+    </div>
   );
 }
