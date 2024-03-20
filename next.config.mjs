108c8cb4a18486c5a3d1ac21d9da765f2e2b31a5
Shahar Yair <shaharyair12@gmail.com>
2024-03-20 03:25:34 +0200

    package: remove bundle analyzer
    - for bundle size reduction

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,3 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -28,4 +24,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+export default nextConfig;
